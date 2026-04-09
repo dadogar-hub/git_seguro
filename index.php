2fd5da1760a52c2ef8f7bbf6c5164ae28a3ae321
dadogar-hub <tnzcyrus@gmail.com>
2026-04-09 11:31:59 +0200

    cambio 2

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
   echo "<h1>Hola Mundo Dadogar 2</h1>";
   echo "<h2>Modificado desde Ahora desde local</h2>";
   echo "<h2>Ahora Firmo</h2>";
-  echo "<h2>Cambio 1</h2>";
+  echo "<h2>Cambio 2</h2>";
 ?>
